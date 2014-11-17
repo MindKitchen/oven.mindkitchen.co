4151ca2a2d2aec70fd73f91b1558d0f749bad96d
Randall A. Gordon <randall@randallagordon.com>
2014-11-17 14:18:02 -0800

    CMYK disco!

--- src/js/app.js
+++ src/js/app.js
@@ -1,60 +1,34 @@
 "use strict";
 var domready = require("domready");
 var raf = require("raf");
-var tinygradient = require("tinygradient");
 var random = require("lodash.random");
 
-window.random = random;
-
-//var colors = [
-  //{ pos: 0.00, color: "cyan" },
-  //{ pos: 0.05, color: "magenta" },
-  //{ pos: 0.10, color: "yellow" },
-//];
-
-var baseColors = ["cyan", "magenta", "yellow"];
-var colors = [];
-var n = 200;
-for (var i = 0; i < n; i++) {
-  colors.push({
-    pos: i * (1 / n),
-    color: baseColors[i % baseColors.length]
-  });
-}
+var color = [
+  "rgba(0, 173, 239, 0.1)",
+  "rgba(236, 0, 139, 0.1)",
+  "rgba(255, 241, 0, 0.1)"
+];
 
 var f = 0;
+
+var width = 200;
+var height = 100;
+
+var ctx = document.getCSSCanvasContext("2d", "mk-animation", width, height);
 
 var renderFrame = function () {
   if (f++ % 2) {
     return raf(renderFrame);
   }
 
-  var gradient = tinygradient(colors);
-
-  //var css = gradient.css("radial", "farthest-corner ellipse at top left");
-  var css = gradient.css("linear", "to bottom");
-  //var css = gradient.css();
-  document.body.style.backgroundImage = css;
-
-  //debugger;
-  var stepSize = 0.001;
-  for (var i = 0; i < colors.length; i++) {
-    var direction = random(1) ? -1 : 1;
-    var step = direction * stepSize;
-
-    var previous = colors[i - 1] ? colors[i - 1].pos : 0;
-    var next = colors[i + 1] ? colors[i + 1].pos : 1;
-
-    if (colors[i].pos + step > previous && colors[i].pos + step < next) {
-      colors[i].pos += step;
-    }
-
-  }
+  ctx.fillStyle = color[random(0, color.length)];
+  ctx.fillRect(0, 0, width, height);
 
   raf(renderFrame);
 };
 
 domready(function () {
-  document.body.style.backgroundAttachment = "fixed";
-  raf(renderFrame);
+  var css = "-webkit-canvas(mk-animation)";
+  document.body.style.background = css;
+  renderFrame();
 });
